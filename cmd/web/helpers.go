21547e5db6ae08a6f0c18f9bb6d60b9e2b9e8c44
skapskap <skaprange@gmail.com>
2023-08-30 19:01:23 -0300

    feat: mais gerenciamento de erros

---
+++ cmd/web/helpers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+// O helper serverError escreve uma mensagem de erro e faz uma stack trace no errorLog,
+// envia um 500 Internal Server Error como resposta ao usuário. O debug.stack() torna possível
+// ver o caminho de execução pra ajudar a debuggar.
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Output(2, trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// O helper clientError envia um código de status específico e a descrição
+// correspondente ao usuário.
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// Notfound Helper. É um wrapper para o clientError que retorna uma resposta
+// de erro 404 para o usuário.
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
