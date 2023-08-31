69cb22ab85c20e87523c6d03d73f1917fca0ed8a
skapskap <skaprange@gmail.com>
2023-08-31 10:16:49 -0300

    feat: estabilizada a conexão com postgres

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
@@ -13,11 +17,39 @@ type application struct {
 }
 
 func main() {
+	err := godotenv.Load("./app.env")
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DBNAME")
+
+	connData := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := sql.Open("postgres", connData)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Database connection test failed:", err)
+	}
+
+	fmt.Println("Connected to the database!")
 
 	app := &application{
 		errorLog: errorLog,
@@ -31,6 +63,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
