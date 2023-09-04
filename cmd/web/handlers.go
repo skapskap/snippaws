fcaafed7124cbf4f49dbb25300b09d1cc7050876
skapskap <skaprange@gmail.com>
2023-09-04 18:39:35 -0300

    feat: renderização inicial dos snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/skapskap/snippaws/pkg/models"
+	"html/template"
 	"net/http"
 	"strconv"
 	"time"
@@ -54,7 +55,24 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", s)
+	data := &templateData{Snippet: s}
+
+	files := []string{
+		"./ui/html/show.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.Execute(w, data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/skapskap/snippaws/pkg/models"
+
+type templateData struct {
+	Snippet *models.Snippet
+}
