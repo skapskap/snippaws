76c19927642a02ffd6cd36a26650552c88436170
skapskap <skaprange@gmail.com>
2023-09-05 11:17:44 -0300

    error handling

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 // O helper serverError escreve uma mensagem de erro e faz uma stack trace no errorLog,
@@ -27,6 +29,14 @@ func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string,
 	td *templateData) {
 	ts, ok := app.templateCache[name]
@@ -35,8 +45,12 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 		return
 	}
 
-	err := ts.Execute(w, td)
+	buf := new(bytes.Buffer)
+
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 	}
+
+	buf.WriteTo(w)
 }
